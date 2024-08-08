55dcbd2fa5919dc84a1aeb76e00b38e835a3c4c6
Kirandeep Gantasala <kirandeepgantasala456@gmail.com>
2024-08-08 07:38:09 +0530

    task 321 is completed

--- User.java
+++ User.java
@@ -5,4 +5,14 @@ class User{
 
         System.out.println(a+b);
     }
+
+    public void m2(){
+        //Some Logic
+
+
+        int a = 10;
+        int b = 20;
+
+        System.out.println(a+b);
+    }
 }
