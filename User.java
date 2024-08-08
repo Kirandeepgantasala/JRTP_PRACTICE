3943ae83fa7ead4c33926abe82fa50ec09744259
Kirandeep Gantasala <kirandeepgantasala456@gmail.com>
2024-08-08 07:41:09 +0530

    m1() is added

--- User.java
+++ User.java
@@ -1,9 +1,16 @@
 class User{
     public static void main(String args[]){
+    
+    }
+    public void m1(){
         int a = 10;
         int b = 20;
 
-        System.out.println(a+b);
+        int c = a + b;
+
+        System.out.println("*************");
+        System.out.println(c);
+        System.out.println("*************");
     }
 
     public void m2(){
